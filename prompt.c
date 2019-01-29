c48ee9ae6caee644bb6143d81c33004881db7a1c
willclarktech <willclarktech@users.noreply.github.com>
2019-01-29 23:29:48 +0100

    Chapter 4: An Interactive Prompt

---
+++ prompt.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#endif
+
+int main(int argc, char** argv) {
+	puts("Lispy version 0.0.0.0.1");
+	puts("Press Ctrl+c to exit\n");
+
+	while (1) {
+		/* Now in either case readline will be correctly defined */
+		char* input = readline("lispy> ");
+		add_history(input);
+
+		printf("No you’re a %s\n", input);
+		free(input);
+	}
+
+	return 0;
+}
---
+++ prompt_unix.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+
+int main(int argc, char** argv) {
+	/* Print version and exit information */
+	puts("Lispy version 0.0.0.0.1");
+	puts("Press Ctrl+c to exit\n");
+
+	/* In a never ending loop */
+	while (1) {
+		/* Output our prompt and get input */
+		char* input = readline("lispy> ");
+
+		/* Add input to history */
+		add_history(input);
+
+		/* Echo input back to user */
+		printf("No you’re a %s\n", input);
+
+		/* Free retrieved input */
+		free(input);
+	}
+
+	return 0;
+}
